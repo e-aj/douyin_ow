b72598752cb550f97857150668edbf8b87230347
xiaoyuej <xiaoyuej@welldeliver.cn>
2022-02-08 15:42:15 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    padding: 0;
+    margin: 0;
 }
-
-#nav {
-  padding: 30px;
+ul,li{
+  display: inline-block;
+  padding: 0;
+    margin: 0;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+li{
+  list-style: none;
 }
 </style>
